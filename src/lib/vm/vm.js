ea155c0fa3d205a3f44a8d50857710c6d0c70efa
medicz <22994538+medicz@users.noreply.github.com>
2023-08-25 13:24:55 +0200

    feat(crypto): add encryption and decryption functions (#20)

--- src/lib/vm/vm.js
+++ src/lib/vm/vm.js
@@ -21,6 +21,7 @@ import { CommitButton } from "../components/Commit";
 import { Typeahead } from "react-bootstrap-typeahead";
 import styled, { isStyledComponent, keyframes } from "styled-components";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { randomBytes, createCipheriv, createDecipheriv } from 'crypto-browserify';
 import Big from "big.js";
 import * as elliptic from "elliptic";
 import BN from "bn.js";
@@ -255,6 +256,7 @@ const Keywords = {
   WebSocket: true,
   VM: true,
   Calimero: true,
+  Crypto: true,
 };
 
 const NativeFunctions = {
@@ -1015,6 +1017,15 @@ class VmStack {
           assertValidObject(obj);
           return obj;
         }
+      } else if (keyword === "Crypto" && callee === "randomBytes") {
+        if (args.length < 1) {
+          throw new Error("Missing argument 'n' for Crypto.randomBytes");
+        }
+        return randomBytes(args[0]);
+      } else if (keyword === "Crypto" && callee === "createCipheriv") {
+        return createCipheriv(...args);
+      } else if (keyword === "Crypto" && callee === "createDecipheriv") {
+        return createDecipheriv(...args);
       } else if (
         (keyword === "State" && callee === "init") ||
         callee === "initState"
